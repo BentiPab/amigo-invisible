c1a84bc5dac171a7f18ca7275c95fe99d87580ed
Pablo Bentivengo <pablo.bentivengo@abstract.it>
2025-12-18 13:41:03 -0300

    add encryption

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,11 +1,21 @@
 const urlScript = import.meta.env.VITE_APPS_SCRIPT_URL;
+const secretWord = import.meta.env.VITE_APPS_SECRET_DECRYPT;
 
 export const getFriends = async () => {
   try {
     const response = await fetch(urlScript);
-    const datos = await response.json();
-    // ... tu lógica
-    return datos;
+    const datosEncriptados = await response.text();
+
+    const decodedBase64 = atob(datosEncriptados);
+
+    let decrypted = "";
+    for (let i = 0; i < decodedBase64.length; i++) {
+      decrypted += String.fromCharCode(
+        decodedBase64.charCodeAt(i) ^
+          secretWord.charCodeAt(i % secretWord.length)
+      );
+    }
+    return JSON.parse(decrypted);
   } catch (error) {
     console.error("Error al conectar con la base de datos", error);
   }
